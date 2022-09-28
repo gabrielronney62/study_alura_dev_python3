4d8ea5414a83aa28500187a4d8993fb5030e5d0c
gabrielronneydasilva <gabriel.r.silva@sesisc.org.br>
2022-09-28 04:17:28 -0300

    Aprendendo Classes,Objetos e Função construtora.

---
+++ orientacao_objetos/conta.py
@@ -0,0 +1,10 @@
+
+class conta():
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        conta.numero = numero
+        conta.titular = titular
+        conta.saldo = saldo
+        conta.limite = limite
+
