81b2dfc9eb67a46c08f44e17cc4a6d8b7f6a7f89
Gabriel Ronney da Silva <gabrielronney62@gmail.com>
2022-10-02 01:52:45 -0300

    Retornando os dados utilizando get, utilização de set para alteração de valores.

--- orientacao_objetos/conta.py
+++ orientacao_objetos/conta.py
@@ -23,3 +23,15 @@ class conta():
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
